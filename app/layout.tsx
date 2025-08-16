4fda7219332a2de4dc4519906ff5a036e8c4dc72
kiv1n <aloha@kiv1n.ru>
2025-08-16 11:10:19 +0300

    Move globals.css to styles directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { MiniAppProvider } from "@/components/mini-app-provider";
+import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
