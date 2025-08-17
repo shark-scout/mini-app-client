cdd375419c7d210ad189d433f18446afa79e6770
kiv1n <aloha@kiv1n.ru>
2025-08-17 11:43:37 +0300

    Add beta tag to header

--- components/header.tsx
+++ components/header.tsx
@@ -14,12 +14,15 @@ export function Header() {
     <header className="sticky top-0 z-40 bg-background border-b">
       <div className="container mx-auto px-4 flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         {/* Left part */}
-        <div className="flex gap-6 md:gap-10">
+        <div className="flex gap-2 md:gap-10">
           <Link href="/" className="flex items-center space-x-3">
             <p className="font-bold">
-              {siteConfig.emoji} {siteConfig.name}
+              {siteConfig.emoji} {siteConfig.name}{" "}
             </p>
           </Link>
+          <div className="bg-accent flex items-center border rounded-lg px-2 py-1">
+            <p className="text-sm font-bold text-accent-foreground">Beta</p>
+          </div>
         </div>
         {/* Right part */}
         <div className="flex flex-1 items-center justify-end gap-4">
