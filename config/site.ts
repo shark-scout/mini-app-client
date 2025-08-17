c27a80874756db85d45ea2f3028bbdd5ad979e0b
kiv1n <aloha@kiv1n.ru>
2025-08-17 13:40:04 +0300

    Update site description in siteConfig

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   emoji: "🦈",
   name: "SharkScout",
-  description: "...", // TODO: Fill in
+  description: "Onchain insights from people you follow",
   links: {
     github: "https://github.com/shark-scout",
     farcaster: "https://farcaster.xyz/kiv1n.eth",
