486a44a56b751b7db2d7a3cde1c97e5e7ff2d463
kiv1n <aloha@kiv1n.ru>
2025-08-29 10:57:24 +0300

    Add TODOs

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,12 @@
 import { appConfig } from "@/config/app";
 
+// TODO: Use Farcaster action to open user's profile
 export function Footer() {
   return (
     <footer className="bg-background border-t py-6 md:py-0">
       <div className="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between gap-4 md:h-24">
         <p className="text-balance text-center md:text-left text-sm leading-loose text-muted-foreground">
-          Built by{" "}
+          Built by
           <a
             href={appConfig.links.farcaster}
             target="_blank"
