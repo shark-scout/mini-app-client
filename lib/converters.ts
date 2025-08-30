e9d2269cb668987bbc01b100bfb5431030ece473
kiv1n <aloha@kiv1n.ru>
2025-08-30 11:07:27 +0300

    Update texts and images for task results

--- lib/converters.ts
+++ lib/converters.ts
@@ -34,26 +34,33 @@ export function balancesUsdValueToDisplayData(
 
   let imagePath = "/images/results/0.png";
   const subtitle1 = `You and your followers collectively own ${valueLocaleString} in crypto`;
-  let subtitle2 = "If you sold it all, you could buy nothing";
+  let subtitle2 = `If you sold it all, you could finally touch grass together and dream of a brighter future 🌱`;
   const postPart1 = `Just realized my followers and I collectively own ${valueLocaleString} in crypto 🤯`;
-  let postPart2 = "If we sold it all, we could buy nothing";
+  let postPart2 = `If we sold it all, we could finally touch grass together and dream of a brighter future 🌱`;
 
   if (value > 0) {
     imagePath = "/images/results/over-0.png";
-    subtitle2 = "If you sold it all, you could buy something";
-    postPart2 = "If we sold it all, we could buy something";
+    subtitle2 = `If you sold it all, you could split a coffee and talk about our next 100x idea`;
+    postPart2 =
+      "If we sold it all, we could split a coffee and talk about our next 100x idea ☕";
   }
 
   if (value > 100) {
     imagePath = "/images/results/over-100.png";
-    subtitle2 = "If you sold it all, you could buy something";
-    postPart2 = "If we sold it all, we could buy something";
+    subtitle2 = `If you sold it all, you could fill a bathtub with lukewarm coffee and call it a "liquidity pool”`;
+    postPart2 = `If we sold it all, we could fill a bathtub with lukewarm coffee and call it a "liquidity pool” ☕`;
   }
 
   if (value > 1000) {
     imagePath = "/images/results/over-1k.png";
-    subtitle2 = `If you sold it all, you could hire a skywriter to draw a giant "gm" over the city`;
-    postPart2 = `If we sold it all, we could hire a skywriter to draw a giant "gm" over the city.`;
+    subtitle2 = `If you sold it all, you could sponsor a local cat shelter for a year and rename all the cats "Satoshi"`;
+    postPart2 = `If we sold it all, we could sponsor a local cat shelter for a year and rename all the cats "Satoshi" 🐈`;
+  }
+
+  if (value > 2500) {
+    imagePath = "/images/results/over-2.5k.png";
+    subtitle2 = `If you sold it all, you could buy enough instant ramen to survive the next bear market in style`;
+    postPart2 = `If we sold it all, we could buy enough instant ramen to survive the next bear market in style 🍜`;
   }
 
   return {
