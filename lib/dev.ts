48aaf7560331d3c0e7693d130a2affa192ab23dc
kiv1n <aloha@kiv1n.ru>
2025-08-31 14:15:14 +0300

    Remove dev lib

--- lib/dev.ts
+++
@@ -1,8 +0,0 @@
-import { devConfig } from "@/config/dev";
-
-export function isUserDev(fid: number | undefined): boolean {
-  if (!fid) {
-    return false;
-  }
-  return devConfig.devUserFids.includes(fid);
-}
