abc4dbc958bf8ade9a2b050a1ba6c18ca30eab45
kiv1n <aloha@kiv1n.ru>
2025-08-31 11:47:49 +0300

    Add premium promotion section and handleGetPremium function in HomeCompletedTask component

--- components/home/home-completed-task.tsx
+++ components/home/home-completed-task.tsx
@@ -3,10 +3,11 @@
 import { appConfig } from "@/config/app";
 import { balancesUsdValueToDisplayData } from "@/lib/converters";
 import { useMiniApp } from "@neynar/react";
-import { ShareIcon } from "lucide-react";
+import { PencilIcon, ShareIcon } from "lucide-react";
 import Image from "next/image";
 import { toast } from "sonner";
 import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
 
 export function HomeCompletedTask(props: {
   balancesUsdValue?: number | string;
@@ -37,6 +38,10 @@ export function HomeCompletedTask(props: {
     }
   }
 
+  async function handleGetPremium() {
+    await actions.viewProfile({ fid: appConfig.developer.fid });
+  }
+
   return (
     <div className="flex flex-col items-center">
       <Image
@@ -55,7 +60,34 @@ export function HomeCompletedTask(props: {
       <Button onClick={handleShareResult} className="mt-6">
         <ShareIcon /> Share The Result
       </Button>
-      {/* Add promo block */}
+      <Separator className="my-6" />
+      <div className="w-full bg-primary rounded-md p-4">
+        <div className="flex flex-col gap-1 text-primary-foreground">
+          <p className="text-2xl font-bold">Go Beyond the Memes!</p>
+          <p className="text-xl font-semibold">Get Real Alpha!</p>
+        </div>
+        <Separator className="my-4" />
+        <div className="flex flex-col gap-4 text-primary-foreground mt-2">
+          <p>
+            SharkScout Premium turns your social graph into an alpha-generating
+            machine{" "}
+          </p>
+          <p>
+            📈 Network Trading Trends: See what your network is buying/selling
+            in real-time
+          </p>
+          <p>
+            🤖 AI Insights: Get signals on which wallets are making the smartest
+            moves
+          </p>
+          <p>
+            🔔 Smart Alerts: Get notified when a key account makes a big trade
+          </p>
+        </div>
+        <Button variant="secondary" onClick={handleGetPremium} className="mt-4">
+          <PencilIcon /> Contact {appConfig.developer.name} To Get Access
+        </Button>
+      </div>
     </div>
   );
 }
