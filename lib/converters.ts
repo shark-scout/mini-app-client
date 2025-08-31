0a5aa97d5fde391403e06ec0b0753452f26cf987
kiv1n <aloha@kiv1n.ru>
2025-08-31 11:15:32 +0300

    Update data for posts

--- lib/converters.ts
+++ lib/converters.ts
@@ -16,7 +16,6 @@ export function errorToString(error: unknown): string {
   return message;
 }
 
-// TODO: Return real data
 export function balancesUsdValueToDisplayData(
   balancesUsdValue?: number | string
 ): {
@@ -61,6 +60,54 @@ export function balancesUsdValueToDisplayData(
     imagePath = "/images/results/over-2.5k.png";
     subtitle2 = `If you sold it all, you could buy enough instant ramen to survive the next bear market in style`;
     postPart2 = `If we sold it all, we could buy enough instant ramen to survive the next bear market in style 🍜`;
+  }
+
+  if (value > 5000) {
+    imagePath = "/images/results/over-5k.png";
+    subtitle2 = `If you sold it all, you could buy a hot dog stand for a day and only accept payments in meme coins`;
+    postPart2 = `If we sold it all, we could buy a hot dog stand for a day and only accept payments in meme coins 🌭`;
+  }
+
+  if (value > 7500) {
+    imagePath = "/images/results/over-7.5k.png";
+    subtitle2 = `If you sold it all, you could sponsor a local llama and give it an official title like "Chief Liquidity Officer"`;
+    postPart2 = `If we sold it all, we could sponsor a local llama and give it an official title like "Chief Liquidity Officer" 🦙`;
+  }
+
+  if (value > 10 * 1000) {
+    imagePath = "/images/results/over-10k.png";
+    subtitle2 = `If you sold it all, you could build a giant, human-sized hamster wheel to help us visualize the endless market cycles`;
+    postPart2 = `If we sold it all, we could build a giant, human-sized hamster wheel to help us visualize the endless market cycles 🎡`;
+  }
+
+  if (value > 25 * 1000) {
+    imagePath = "/images/results/over-25k.png";
+    subtitle2 = `If you sold it all, you could buy a mechanical bull and install it at the next crypto conference`;
+    postPart2 = `If we sold it all, we could buy a mechanical bull and install it at the next crypto conference 🐂`;
+  }
+
+  if (value > 50 * 1000) {
+    imagePath = "/images/results/over-50k.png";
+    subtitle2 = `If you sold it all, you could rent a billboard in Times Square for 30 minutes to display our best shitposts`;
+    postPart2 = `If we sold it all, we could rent a billboard in Times Square for 30 minutes to display our best shitposts 🌆`;
+  }
+
+  if (value > 100 * 1000) {
+    imagePath = "/images/results/over-100k.png";
+    subtitle2 = `If you sold it all, you could build a giant statue of the "this is fine" dog and place it outside the SEC's office`;
+    postPart2 = `If we sold it all, we could build a giant statue of the "this is fine" dog and place it outside the SEC's office 🔥`;
+  }
+
+  if (value > 500 * 1000) {
+    imagePath = "/images/results/over-500k.png";
+    subtitle2 = `If you sold it all, you could attempt to buy the naming rights to a newly discovered star and call it "gm"`;
+    postPart2 = `If we sold it all, we could attempt to buy the naming rights to a newly discovered star and call it "gm" ⭐`;
+  }
+
+  if (value > 1000 * 1000) {
+    imagePath = "/images/results/over-1m.png";
+    subtitle2 = `If you sold it all, you could buy a retired F-16 fighter jet and paint it purple with a giant "gm" on the tail`;
+    postPart2 = `If we sold it all, we could buy a retired F-16 fighter jet and paint it purple with a giant "gm" on the tail ✈️`;
   }
 
   return {
