fa890cab9c8ff275ac7c566d06ccc0f879b09c6b
kiv1n <aloha@kiv1n.ru>
2026-01-19 14:31:42 +0300

    Update Shark Battles component with new imagery, title, and button text

--- components/sharkbattles/sharkbattles.tsx
+++ components/sharkbattles/sharkbattles.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { posthogConfig } from "@/config/posthog";
 import { useMiniApp } from "@neynar/react";
-import { WandSparklesIcon } from "lucide-react";
+import { Swords } from "lucide-react";
 import Image from "next/image";
 import posthog from "posthog-js";
 
@@ -21,8 +21,8 @@ export default function SharkBattles() {
     <div className="h-[calc(100vh-6rem)] max-w-xl mx-auto px-4 py-8 flex flex-col items-center justify-center">
       {/* Image */}
       <Image
-        src="/images/shark-ids.png"
-        alt="Shark IDs"
+        src="/images/sharkbattles.png"
+        alt="Shark Battles"
         priority={false}
         width="100"
         height="100"
@@ -30,21 +30,21 @@ export default function SharkBattles() {
         className="w-full rounded-md"
       />
       {/* Title, subtitle */}
-      <h2 className="text-3xl text-accent font-bold tracking-tight balancetext-center mt-6">
-        Mint your Shark ID!
+      <h2 className="text-3xl text-accent font-bold tracking-tight text-center mt-6">
+        24-hour Shark Battle is&nbsp;LIVE!
       </h2>
       <h4 className="text-xl tracking-tight text-center mt-2">
-        Discover your shark archetype, unlock your 2026 prediction, and join the
-        Shark Battles airdrop!
+        Join the battle for USDC rewards sponsored by Celebration Hub!
       </h4>
+      {/* Button */}
       <Button
         variant="default"
         size="lg"
         className="mt-6"
         onClick={handleOpenSharkBattles}
       >
-        <WandSparklesIcon />
-        Mint Shark ID & Join Airdrop!
+        <Swords />
+        Join battle!
       </Button>
     </div>
   );
